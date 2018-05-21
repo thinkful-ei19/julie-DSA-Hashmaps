b414102b82973354de9297fb016ef86fe6753b46
Julie K <julieskim160@gmail.com>
2018-05-21 09:19:13 -0700

    lor and get maiar

--- drills.js
+++ drills.js
@@ -80,3 +80,23 @@ class HashMap {
 
 HashMap.MAX_LOAD_RATIO = 0.9;
 HashMap.SIZE_RATIO = 3;
+
+function main() {
+   let lor = new HashMap(); 
+    lor.set('Hobbit', 'Bilbo');
+    lor.set('Hobbit', 'Frodo');
+    lor.set('Wizard', 'Gandolf');
+    lor.set('Human', 'Aragon');
+    lor.set('Elf', 'Legolas');
+    lor.set('Maiar', 'The Necromancer');
+    lor.set('Maiar', 'Sauron');
+    lor.set('RingBearer', 'Gollum');
+    lor.set('LadyOfLight', 'Galadriel');
+    lor.set('HalfElven', 'Arwen');
+    lor.set('Ent', 'Treebeard');
+    // console.log(lor);
+    console.log(lor.get('Maiar'));
+}
+
+
+main();
